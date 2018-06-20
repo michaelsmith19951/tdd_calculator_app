7b274cda38552846dca2b2b4d92b38f4ab7c08bf
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-20 16:10:31 -0400

    Eighth test of calculator app that passed

--- test_tdd_calculator_app.rb
+++ test_tdd_calculator_app.rb
@@ -13,7 +13,7 @@ class TestTddCalculatorApp < Minitest::Test
        assert_equal(30, addition(20, 10))
     end
     def test_assert_that_100_plus_100_equals_200
-       assert_equal(5, addition(100, 100))
+       assert_equal(200, addition(100, 100))
     end
     def test_assert_that_20_minus_5_equals_15
        assert_equal(15, subtraction(20, 5))
