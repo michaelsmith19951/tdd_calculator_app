754596735de01b43a63c1c52b89cdccb617a7d69
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-20 16:11:45 -0400

    Ninth test of calculator app that failed

--- test_tdd_calculator_app.rb
+++ test_tdd_calculator_app.rb
@@ -15,6 +15,9 @@ class TestTddCalculatorApp < Minitest::Test
     def test_assert_that_100_plus_100_equals_200
        assert_equal(200, addition(100, 100))
     end
+     def test_assert_that_200_plus_200_equals_400
+       assert_equal(5, addition(200, 200))
+    end
     def test_assert_that_20_minus_5_equals_15
        assert_equal(15, subtraction(20, 5))
     end
