0e861b4c0628772d1c76f1ad4f30e7789030c9a7
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-20 16:01:14 -0400

    Fifth test of calculator app that failed

--- test_tdd_calculator_app.rb
+++ test_tdd_calculator_app.rb
@@ -9,6 +9,9 @@ class TestTddCalculatorApp < Minitest::Test
     def test_assert_that_1_plus_4_equals_5
        assert_equal(5, addition(1, 4))
     end
+    def test_assert_that_20_plus_10_equals_30
+       assert_equal(2, addition(20, 10))
+    end
     def test_assert_that_20_minus_5_equals_15
        assert_equal(15, subtraction(20, 5))
     end
